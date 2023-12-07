d0a5d53b7f932c14bfb53fe2cbab2ca8a986a8aa
Meredith <124798853+Mer-W@users.noreply.github.com>
2023-12-07 15:48:37 -0500

    models for comment and like
    -- may need to be revised -- untested because they reference models that do not yet exist

---
+++ backend/src/database/models/Comment.ts
@@ -0,0 +1,83 @@
+import User from './User';
+import Post from './Post';
+import Like from './Like';
+
+
+import {
+    Table,
+    Column,
+    Model,
+    DataType,
+    ForeignKey,
+    BelongsTo,
+    HasMany
+
+} from 'sequelize-typescript';
+
+@Table({
+    timestamps: true,
+    tableName: 'comment',
+})
+class Comment extends Model {
+
+    @Column({
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    })
+    id!: number;
+
+    // author of comment
+    @ForeignKey(() => User)
+    @Column({
+        type: DataType.INTEGER,
+        allowNull: false,
+    })
+    userId!: number;
+
+    // top-level post
+    @ForeignKey(() => Post)
+    @Column({
+        type: DataType.INTEGER,
+        allowNull: false,
+    })
+    postId!: number;
+
+    // parent comment if nested
+    @ForeignKey(() => Comment)
+    @Column({
+        type: DataType.INTEGER,
+        allowNull: false,
+    })
+    parentId?: number;
+
+    // comment body
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    body!: string;
+
+    // like count
+    @Column({
+        type: DataType.INTEGER,
+        allowNull: false,
+    })
+    likeCount!: number;
+
+    //model associations
+
+    @BelongsTo(() => User)
+    user: User;
+
+    @BelongsTo(() => Post)
+    post: Post;
+
+    @BelongsTo(() => Comment)
+    comment?: Comment;
+
+    @HasMany(() => Like)
+    likes?: Like[];
+}
+
+export default Comment;
