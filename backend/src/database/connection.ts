d9ba4be4272acba04d842c018053ced105e1507f
Alex Franklin <alexandra.a.franklin@gmail.com>
2023-12-07 14:24:15 -0500

    db connection set up

---
+++ backend/src/database/connection.ts
@@ -0,0 +1,36 @@
+import { Sequelize } from "sequelize-typescript";
+import dotenv from "dotenv";
+dotenv.config();
+
+const dbName = process.env.DB_NAME;
+const dbUsername = process.env.DB_USERNAME;
+const dbPassword = process.env.DB_PASSWORD;
+const dbHost = process.env.DB_HOST;
+const dbPort = Number(process.env.DB_PORT);
+
+if (!dbName || !dbUsername || !dbPassword || !dbHost || isNaN(dbPort)) {
+  throw new Error(
+    "One or more required environment variables are undefined or invalid."
+  );
+}
+
+const sequelize = new Sequelize({
+  database: dbName,
+  dialect: "mysql",
+  username: dbUsername,
+  password: dbPassword,
+  host: dbHost,
+  port: dbPort,
+  models: [__dirname + "/models"],
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Database connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
+
+export default sequelize;
