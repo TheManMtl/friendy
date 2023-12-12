4ca5321b4c567b6b577e10295bfbc3dc0dfc3b6c
Alex Franklin <alexandra.a.franklin@gmail.com>
2023-12-12 09:48:22 -0500

    changed app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,11 +2,8 @@ import dotenv from "dotenv";
 import express, { Request, Response, NextFunction } from "express";
 import userRoutes from "./routes/Users-routes";
 import friendRoutes from "./routes/friends-routes";
-<<<<<<< Updated upstream
 import postRoutes from "./routes/posts-routes";
-=======
 import profileRoutes from "./routes/profile-routes";
->>>>>>> Stashed changes
 import session from "express-session";
 
 dotenv.config();
@@ -27,16 +24,14 @@ app.use(
 );
 
 app.get("/", async (req, res) => {
+  console.log("hello world");
   res.send("Hello there, world");
 });
 
 app.use("/api/users", userRoutes);
 app.use("/api/friends", friendRoutes);
-<<<<<<< Updated upstream
 app.use("/api/posts", postRoutes);
-=======
 app.use("/api/profile", profileRoutes);
->>>>>>> Stashed changes
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
   console.error(error);
