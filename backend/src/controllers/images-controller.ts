a78d4af0c2dbf4ad4247f3d0bc06237ae6f16677
wang5322 <124798791+wang5322@users.noreply.github.com>
2023-12-13 22:39:53 -0500

    album and image

--- backend/src/controllers/images-controller.ts
+++ backend/src/controllers/images-controller.ts
@@ -1,4 +1,5 @@
-import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { PutObjectCommand, GetObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import crypto from 'crypto';
 import sharp from 'sharp';
 import models from "../db/models";
@@ -48,4 +49,48 @@ export const add = async (req: any, res: any) => {
     }
 };
 
+export const getOne = async (req: any, res: any) => {
+    const image = await models.Image.findOne({
+        where: {
+            id: req.body.imageId
+        }
+    });
+    const GetObjectParams = {
+        Bucket: req.bucketName,
+        Key: image.fileName
+    }
+    const GetThumbnailParams = {
+        Bucket: req.bucketName,
+        Key: image.thumbnail
+    }
+    const command = new GetObjectCommand(GetObjectParams);
+    const thumbnailCommand = new GetObjectCommand(GetThumbnailParams);
+    const url = await getSignedUrl(req.s3, command, { expiresIn: 3600 });
+    const thumbnailUrl = await getSignedUrl(req.s3, thumbnailCommand, { expiresIn: 3600 });
+    image.imageUrl = url;
+    image.thumbnailUrl = thumbnailUrl;
+    res.send(image);
+};
+
+export const remove = async (req: any, res: any) => {
+    const image = await models.Image.findOne({
+        where: {
+            id: req.body.imageId
+        }
+    });
+    const deleteObjectParams = {
+        Bucket: req.bucketName,
+        Key: image.fileName
+    }
+    const deleteThumbnailParams = {
+        Bucket: req.bucketName,
+        Key: image.thumbnail
+    }
+    const command = new DeleteObjectCommand(deleteObjectParams);
+    const thumbnailCommand = new DeleteObjectCommand(deleteThumbnailParams);
+    await req.s3.send(command);
+    await req.s3.send(thumbnailCommand);
+    await image.destroy();
+    res.status(204).json({ message: "Image deleted"});
+}
 
