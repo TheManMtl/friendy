7ada0604282be81da9ab1de2e53b2ced84f73ac3
Ehsan Khoshnavaz Motlagh <e.khoshnavaz@gmail.com>
2023-12-18 13:41:23 -0500

    Update deploy.sh

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,3 +1,3 @@
  docker build -t front -f ./frontend/Dockerfile .
 
- docker compose up -f ./frontend/docker-compose.yml -d --remove-orphans
+ docker compose -f ./frontend/docker-compose.yml up -d --remove-orphans
