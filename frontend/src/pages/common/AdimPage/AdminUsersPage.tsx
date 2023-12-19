45c99f875087d50d4593d822e74557f7c9d662a1
Ehsan Khoshnavaz Motlagh <e.khoshnavaz@gmail.com>
2023-12-18 22:16:54 -0500

    Admin nav

--- frontend/src/pages/common/AdimPage/AdminUsersPage.tsx
+++ frontend/src/pages/common/AdimPage/AdminUsersPage.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import AdminNavbarTop from './AdminComponents/AdminNavbarTop'
+import React from 'react';
+import AdminNavbarTop from './AdminComponents/AdminNavbarTop';
+import "./AdminStyle.css";
 function AdminUsersPage() {
   return (
     <>
-      <p>Admin Panel</p>
+      <div className="row bg-pink">
+        <h1 className='mt-3'>Admin Panel</h1>
+      </div>
       <div className="row">
 
       </div>
       <AdminNavbarTop />
-      <div>AdminUsersPage</div>
+      <div className='mt-3'>AdminUsersPage</div>
 
     </>
   )
