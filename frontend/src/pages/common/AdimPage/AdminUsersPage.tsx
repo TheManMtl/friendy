2575c2ef4e6af2055cb36ec57d5187e242cee905
Ehsan Khoshnavaz Motlagh <e.khoshnavaz@gmail.com>
2023-12-18 22:49:25 -0500

    users in table

--- frontend/src/pages/common/AdimPage/AdminUsersPage.tsx
+++ frontend/src/pages/common/AdimPage/AdminUsersPage.tsx
@@ -1,20 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'; // Import axios
 import AdminNavbarTop from './AdminComponents/AdminNavbarTop';
 import "./AdminStyle.css";
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  // Add more properties as needed
+}
+
 function AdminUsersPage() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    // Fetch users from your backend API using axios
+    axios.get("http://localhost:8080/api/users/admin")
+      .then(response => setUsers(response.data))
+      .catch(error => console.error('Error fetching users:', error));
+  }, []);
+
   return (
     <>
       <div className="row bg-pink">
         <h1 className='mt-3'>Admin Panel</h1>
       </div>
-      <div className="row">
-
-      </div>
       <AdminNavbarTop />
-      <div className='mt-3'>AdminUsersPage</div>
 
+      <div className="container mt-3">
+        <h2>Users List</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              {/* Add more columns as needed */}
+            </tr>
+          </thead>
+          <tbody>
+            {users.map(user => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                {/* Add more columns as needed */}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
-  )
+  );
 }
 
-export default AdminUsersPage
+export default AdminUsersPage;
