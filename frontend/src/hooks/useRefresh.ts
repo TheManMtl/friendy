1101f809156816b27d086c07842922e9e7cb9d86
Meredith <124798853+Mer-W@users.noreply.github.com>
2023-12-21 16:21:52 -0500

    more on comments and auth error handling

--- frontend/src/hooks/useRefresh.ts
+++ frontend/src/hooks/useRefresh.ts
@@ -1,24 +1,32 @@
 import useAuth from "../hooks/useAuth";
 import axios from "../services/api/axios";
+import { useNavigate } from "react-router-dom";
 
 const useRefresh = () => {
 
     const { setUser } = useAuth();
+    const navigate = useNavigate();
 
     const refresh = async () => {
 
-        //use default axios instance
-        //append cookies containing refresh token
-        const response = await axios.get('/users/refresh',
-            {
-                withCredentials: true,
+        try {
+            // use default axios instance
+            // append cookies containing refresh token
+            const response = await axios.get('/users/refresh', {
+              withCredentials: true,
             });
+      
+            // replace user in auth context
+            setUser(response.data);
+      
+            // return token only for export
+            return response.data.token;
 
-        //replace user in auth context
-        setUser(response.data);
-
-        //return token only for export
-        return response.data.token;
+          } catch (error) {
+            // nullify user and redirect to login
+            setUser(null);
+            navigate("/login");
+          }
     }
 
     return refresh;
