0b7c68c4be6b166b98c8b838f9e4caca6b876ebe
Alex Franklin <alexandra.a.franklin@gmail.com>
2024-01-08 14:41:09 -0500

    update password

--- backend/src/routes/Users-routes.ts
+++ backend/src/routes/Users-routes.ts
@@ -6,11 +6,11 @@ import cookieParser from "cookie-parser";
 const router = express.Router();
 router.use(cookieParser());
 
-
 router.post("/signup", UserController.signUp);
 router.post("/login", UserController.login);
 router.delete("/logout", auth.authUser, auth.logout);
 router.get("/refresh", UserController.refresh);
 router.get("/admin", UserController.all);
+router.put("/password", auth.authUser, UserController.changedPassword);
 
 export default router;
