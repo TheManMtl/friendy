29bca339082c4856ab557ebf0fb02bc2547a04ff
Alex Franklin <alexandra.a.franklin@gmail.com>
2024-01-08 19:58:55 -0500

    workingn on comments moooore

--- frontend/src/components/common/Comment/Comment.tsx
+++ frontend/src/components/common/Comment/Comment.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import "./Comment.css";
 import useAxiosToken from "../../../hooks/useAxiosToken";
@@ -15,6 +15,7 @@ import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en";
 import CommentContainer from "./CommentContainer";
 import { axiosToken } from "../../../services/api/axios";
+import useAuth from "../../../hooks/useAuth";
 type CommentProps = {
   isNested: boolean;
   comment: Comments;
@@ -23,11 +24,14 @@ type CommentProps = {
 const Comment: React.FC<CommentProps> = ({ isNested, comment }) => {
   const [viewReplies, setViewReplies] = useState<boolean>(false);
   const [makeComment, setMakeComment] = useState<boolean>(false);
+  const [editComment, setEditComment] = useState<boolean>(false);
   const [replies, setReplies] = useState<number[]>([]);
   const [newComments, setNewComments] = useState<Comments[]>([]);
   const [userLiked, setUserLiked] = useState<boolean>(false);
   const [theComment, setTheComment] = useState<Comments>(comment);
   const [commentChanged, setCommentChanges] = useState<boolean>(false);
+  const [firstRender, setFirstRender] = useState<boolean>(true);
+
   TimeAgo.addDefaultLocale(en);
   const timeAgo = new TimeAgo("en-US");
 
@@ -36,8 +40,24 @@ const Comment: React.FC<CommentProps> = ({ isNested, comment }) => {
     setReplies(updatedReplies);
   };
 
+  const handleLike = async () => {
+    console.log("LIKE");
+    // router.post("/comment/:id([0-9]+)", authUser, likes.likeCommentToggle);
+    await axiosToken
+      .post(`${process.env.REACT_APP_HOST_URL}/likes/comment/${comment.id}`)
+      .then((response) => {
+        console.log(response.data);
+        console.log("is this working?");
+        setCommentChanges(!commentChanged);
+      })
+      .catch((error) => {
+        console.log("is this NOT working?");
+        console.log(error);
+      });
+  };
+
   useEffect(() => {
-    console.log("comment changed");
+    console.log("user:  ");
     const fetchLikes = async () => {
       await axiosToken
         .get(`${process.env.REACT_APP_HOST_URL}/likes/comment/${comment.id}`)
@@ -49,9 +69,24 @@ const Comment: React.FC<CommentProps> = ({ isNested, comment }) => {
           console.log(error);
         });
     };
-
+    const fetchNewComment = async () => {
+      await axiosToken
+        .get(`${process.env.REACT_APP_HOST_URL}/comments/single/${comment.id}`)
+        .then((response) => {
+          setTheComment(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
     fetchLikes();
-  }, [commentChanged]);
+    if (!firstRender) {
+      fetchNewComment();
+    } else {
+      setFirstRender(false);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [comment.id, commentChanged]);
 
   useEffect(() => {
     const fetchComments = async () => {
@@ -79,75 +114,139 @@ const Comment: React.FC<CommentProps> = ({ isNested, comment }) => {
   }, [replies]);
 
   return (
-    <div
-      className={`d-flex flex-lg-row flex-column my-3 pe-2 nested-${isNested} `}
-    >
-      {theComment.profileImg && (
-        <div className="">
-          <ProfileImage
-            src={theComment.profileImg!}
-            alt={"alt"}
-            size={"small-small-med"}
-          />
-        </div>
-      )}
+    <div>
+      <div
+        className={`d-flex flex-lg-row flex-column my-3 pe-2 nested-${isNested} `}
+      >
+        {theComment.profileImg && !editComment && (
+          <div className="">
+            <ProfileImage
+              src={theComment.profileImg!}
+              alt={"alt"}
+              size={"small-small-med"}
+            />
+          </div>
+        )}
 
-      <div className=" flex-grow-1">
-        <div className="row text-start bg-comment ms-1">
-          <div className="col-12  fw-bolder">
-            {theComment.name && theComment.name}
-          </div>
-          <div className="col-12 ">{theComment.body && theComment.body}</div>
-        </div>
-        <div className="row text-start  ms-1">
-          <div className="col-12">
-            <span className="comment-interaction me-2">Like</span>
-            <span
-              className={`comment-interaction  me-2 comment-${makeComment}`}
-              onClick={() => {
-                setMakeComment(!makeComment);
-              }}
-            >
-              Comment
-            </span>
-            <span className="comment-interaction me-2">
-              {theComment.createdAt &&
-                timeAgo.format(new Date(theComment.createdAt))}
-            </span>
-            <span className="comment-interaction ">
-              <HandThumbsUpFill />{" "}
-              {theComment.likeCount && <>{theComment.likeCount}</>}
-            </span>
-          </div>
-          {theComment.childCount > 0 && !makeComment && (
-            <div className="col-12 view-replies">
-              {theComment.childCount > 0 && (
-                <div
-                  onClick={() => {
-                    setViewReplies(!viewReplies);
-                    console.log(theComment.id);
-                  }}
-                >
-                  {viewReplies ? <>Close Replies</> : <>View Replies</>}
-                </div>
-              )}
+        <div className=" flex-grow-1">
+          {!editComment && (
+            <div className="row text-start bg-comment ms-1">
+              <div className="col-12  fw-bolder">
+                {theComment.name && !editComment && theComment.name}
+              </div>
+              <div className="col-12 ">
+                {theComment.body && !editComment && theComment.body}
+              </div>
             </div>
           )}
-          {makeComment && (
-            <CommentReply
-              commentId={theComment.id}
-              newComment={newComment}
-              setMakeComment={setMakeComment}
-            />
+
+          {editComment && (
+            <div className="row text-start ms-1">
+              <div className="col-12">
+                <CommentReply
+                  commentId={theComment.id}
+                  newComment={newComment}
+                  setMakeComment={setEditComment}
+                  isNewComment={true}
+                  commentChanged={commentChanged}
+                  setCommentChanges={setCommentChanges}
+                />
+              </div>
+              <div
+                className="comment-interaction fw-bolder col-12 text-end cancel-edit"
+                onClick={() => {
+                  setEditComment(!editComment);
+                }}
+              >
+                Cancel
+              </div>
+            </div>
           )}
-          {replies.length > 0 &&
-            newComments.map((comment, index) => (
-              <Comment key={index} isNested={true} comment={comment} />
-            ))}
-          {comment.childCount > 0 && viewReplies && (
-            <CommentContainer commentId={comment.id} postId={comment.postId} />
-          )}
+          <div className="row text-start  ms-1">
+            {!editComment && (
+              <div className="col-12">
+                <span
+                  className={`comment-interaction me-1 user-liked-${userLiked}`}
+                  onClick={handleLike}
+                >
+                  {userLiked ? <>Unlike</> : <>Like</>}
+                </span>
+                <span
+                  className={`comment-interaction  me-1 comment-${makeComment}`}
+                  onClick={() => {
+                    setMakeComment(!makeComment);
+                  }}
+                >
+                  Comment
+                </span>
+                <span className="comment-interaction me-1">
+                  {theComment.createdAt &&
+                    timeAgo.format(new Date(theComment.createdAt))}
+                </span>
+                <span className="comment-interaction ">
+                  <HandThumbsUpFill />{" "}
+                  {theComment.likeCount && <>{theComment.likeCount}</>}
+                </span>
+              </div>
+            )}
+
+            {theComment && (
+              <div className="col-12">
+                <span
+                  onClick={() => {
+                    setEditComment(!editComment);
+                  }}
+                  className="comment-interaction fw-bolder"
+                >
+                  {!editComment && <>Edit</>}
+                </span>
+                <span className="comment-interaction fw-bolder">
+                  {!editComment && <>Delete</>}
+                </span>
+              </div>
+            )}
+            {theComment.childCount > 0 && !makeComment && (
+              <div className="col-12 view-replies">
+                {theComment.childCount > 0 && !editComment && (
+                  <div
+                    onClick={() => {
+                      setViewReplies(!viewReplies);
+                      console.log(theComment.id);
+                    }}
+                  >
+                    {viewReplies ? <>Close Replies</> : <>View Replies</>}
+                  </div>
+                )}
+              </div>
+            )}
+          </div>
         </div>
+      </div>
+      <div className="nested-in-comment">
+        {makeComment && (
+          <CommentReply
+            commentId={theComment.id}
+            newComment={newComment}
+            setMakeComment={setMakeComment}
+            isNewComment={false}
+            commentChanged={commentChanged}
+            setCommentChanges={setCommentChanges}
+          />
+        )}
+
+        {replies.length > 0 &&
+          newComments.map((comment, index) => (
+            <Comment key={index} isNested={true} comment={comment} />
+          ))}
+        {comment.childCount > 0 && viewReplies && (
+          <div className="nested-in-comment">
+            {" "}
+            <CommentContainer
+              commentId={comment.id}
+              postId={comment.postId}
+            />{" "}
+          </div>
+        )}
       </div>
     </div>
   );
