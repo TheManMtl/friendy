f3f99a63c1d33226e51b5604f9ee548956f459ee
ShiyuCCC <115126635+shiyuCCC@users.noreply.github.com>
2024-01-10 09:25:34 -0500

    hooked photos to post image detail page
    hooked photos to post image detail page

--- frontend/src/components/common/ProfilePage/PhotoGallery.tsx
+++ frontend/src/components/common/ProfilePage/PhotoGallery.tsx
@@ -3,6 +3,7 @@ import "../../../pages/common/ProfilePage/ProfilePage.css";
 import useAxiosToken from "../../../hooks/useAxiosToken";
 import Axios from "axios";
 import { IPost } from "../../../pages/shared/interface/post.interface";
+import { useNavigate } from "react-router-dom";
 
 interface PhotoGalleryProps {
   userParamId: string | undefined;
@@ -10,9 +11,13 @@ interface PhotoGalleryProps {
 
 const PhotoGallery: React.FC<PhotoGalleryProps> = ({ userParamId }) => {
   const axiosToken = useAxiosToken();
-  const [imageUrlArr, setImageUrlArr] = useState<string[]>();
-  const [thumbUrlArr, setThumbUrlArr] = useState<string[]>();
-  //fetch first 9 posts with the authroId=userId imageId !=null
+  const [imagePosts, setImagePost] = useState<IPost[]>();
+  const navigate = useNavigate();
+
+  // const displayPostDetail = () => {
+  //   navigate(`/display?postid=${props.postId}`);
+  // };
+
   useEffect(() => {
     if (userParamId !== undefined) {
       axiosToken
@@ -20,16 +25,17 @@ const PhotoGallery: React.FC<PhotoGalleryProps> = ({ userParamId }) => {
         .then((response) => {
           if (response.data.length > 0) {
             //Take the first 9 imageUrl
-            const first9Images = response.data.slice(0, 9);
+            const first9Images: IPost[] = response.data.slice(0, 9);
+            setImagePost(first9Images);
             //Extract imageUrl values and update imageUrlArr
-            const imageUrlValues = first9Images.map(
-              (item: IPost) => item.imageUrl
-            );
-            const thumbUrlValues = first9Images.map(
-              (item: IPost) => item.thumbnailUrl
-            );
-            setImageUrlArr(imageUrlValues);
-            setThumbUrlArr(thumbUrlValues);
+            // const imageUrlValues = first9Images.map(
+            //   (item: IPost) => item.imageUrl && item.postId
+            // );
+            // const thumbUrlValues = first9Images.map(
+            //   (item: IPost) => item.thumbnailUrl && item.postId
+            // );
+            // setImageUrlArr(imageUrlValues);
+            // setThumbUrlArr(thumbUrlValues);
           }
         })
         .catch((error) => {
@@ -47,24 +53,23 @@ const PhotoGallery: React.FC<PhotoGalleryProps> = ({ userParamId }) => {
         <div className="row justify-content-center">
           <div className="col-md-8">
             <div className="row">
-              {imageUrlArr && imageUrlArr.length > 0
-                ? imageUrlArr.slice(0, 3).map((imageUrl, index) => (
-                    <a
-                      key={index}
-                      href={imageUrl}
-                      data-toggle="lightbox"
-                      data-gallery="example-gallery"
-                      className="col-sm-4"
+              {imagePosts && imagePosts.length > 0
+                ? imagePosts.slice(0, 3).map((imagePost, index) => (
+                    <div
+                      onClick={() => {
+                        navigate(`/display?postid=${imagePost.id}`);
+                      }}
+                      className="col-sm-4 curser-pointer"
                     >
                       <img
-                        src={imageUrl}
+                        src={imagePost.imageUrl}
                         className="img-fluid"
                         alt={`gallery-${index}`}
                       />
-                    </a>
+                    </div>
                   ))
                 : Array.from({
-                    length: Math.max(0, 3 - (imageUrlArr?.length ?? 0)),
+                    length: Math.max(0, 3 - (imagePosts?.length ?? 0)),
                   }).map((_, index) => (
                     <div key={index} className="col-sm-4">
                       {/* Fallback content or empty */}
@@ -72,24 +77,23 @@ const PhotoGallery: React.FC<PhotoGalleryProps> = ({ userParamId }) => {
                   ))}
             </div>
             <div className="mt-3 row">
-              {imageUrlArr && imageUrlArr.length > 3
-                ? imageUrlArr.slice(3, 6).map((imageUrl, index) => (
-                    <a
-                      key={index}
-                      href={imageUrl}
-                      data-toggle="lightbox"
-                      data-gallery="example-gallery"
-                      className="col-sm-4"
+              {imagePosts && imagePosts.length > 3
+                ? imagePosts.slice(3, 6).map((imagePost, index) => (
+                    <div
+                      onClick={() => {
+                        navigate(`/display?postid=${imagePost.id}`);
+                      }}
+                      className="col-sm-4 curser-pointer"
                     >
                       <img
-                        src={imageUrl}
+                        src={imagePost.imageUrl}
                         className="img-fluid"
                         alt={`gallery-${index}`}
                       />
-                    </a>
+                    </div>
                   ))
                 : Array.from({
-                    length: Math.max(0, 3 - (imageUrlArr?.length ?? 0)),
+                    length: Math.max(0, 3 - (imagePosts?.length ?? 0)),
                   }).map((_, index) => (
                     <div key={index} className="col-sm-4">
                       {/* Fallback content or empty */}
@@ -97,24 +101,23 @@ const PhotoGallery: React.FC<PhotoGalleryProps> = ({ userParamId }) => {
                   ))}
             </div>
             <div className="mt-3 row">
-              {imageUrlArr && imageUrlArr.length > 6
-                ? imageUrlArr.slice(6, 9).map((imageUrl, index) => (
-                    <a
-                      key={index}
-                      href={imageUrl}
-                      data-toggle="lightbox"
-                      data-gallery="example-gallery"
-                      className="col-sm-4"
+              {imagePosts && imagePosts.length > 6
+                ? imagePosts.slice(6, 9).map((imagePost, index) => (
+                    <div
+                      onClick={() => {
+                        navigate(`/display?postid=${imagePost.id}`);
+                      }}
+                      className="col-sm-4 curser-pointer"
                     >
                       <img
-                        src={imageUrl}
+                        src={imagePost.imageUrl}
                         className="img-fluid"
                         alt={`gallery-${index}`}
                       />
-                    </a>
+                    </div>
                   ))
                 : Array.from({
-                    length: Math.max(0, 3 - (imageUrlArr?.length ?? 0)),
+                    length: Math.max(0, 3 - (imagePosts?.length ?? 0)),
                   }).map((_, index) => (
                     <div key={index} className="col-sm-4">
                       {/* Fallback content or empty */}
--- frontend/src/pages/common/ProfilePage/ProfilePagePhoto.tsx
+++ frontend/src/pages/common/ProfilePage/ProfilePagePhoto.tsx
@@ -1,8 +1,7 @@
-
 import React, { useContext, useEffect, useState } from "react";
 import "./PhotoPage.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import { Dropdown } from 'flowbite-react';
+import { Dropdown } from "flowbite-react";
 import { IPost } from "../../shared/interface/post.interface";
 import { AuthContext } from "../../../context/AuthProvider";
 import { useNavigate } from "react-router-dom";
@@ -18,14 +17,12 @@ interface ProfilePagePhotoProps {
 const ProfilePagePhoto: React.FC<ProfilePagePhotoProps> = ({ profileId }) => {
   const [posts, setPosts] = useState<IPost[]>([]);
   const [alertMessage, setAlertMessage] = useState<string | null>(null);
- const axiosToken = useAxiosToken();
+  const axiosToken = useAxiosToken();
 
   useEffect(() => {
-    axiosToken
-      .get(`/posts/user/${profileId}/photos`)
-      .then((res) => {
-        setPosts(res.data);
-      });
+    axiosToken.get(`/posts/user/${profileId}/photos`).then((res) => {
+      setPosts(res.data);
+    });
   }, []);
   const showAlert = (message: string) => {
     setAlertMessage(message);
@@ -33,28 +30,33 @@ const ProfilePagePhoto: React.FC<ProfilePagePhotoProps> = ({ profileId }) => {
   return (
     <div>
       {alertMessage && (
-        <Alert variant="success" onClose={() => setAlertMessage(null)} dismissible>
+        <Alert
+          variant="success"
+          onClose={() => setAlertMessage(null)}
+          dismissible
+        >
           {alertMessage}
         </Alert>
       )}
       <div className="container">
-      <div className="row">
-      {posts.map((post) => (
-        <div key={`post-${post.id}`} className="col-lg-4 col-md-12 mb-4 mb-lg-0 position-relative">
-          <PostImage
-           postId={post.id}
-            thumbnailUrl={post.thumbnailUrl}
-            alt={"thumbnail"}
-            showAlert={showAlert}
-          />
+        <div className="row">
+          {posts.map((post) => (
+            <div
+              key={`post-${post.id}`}
+              className="col-lg-4 col-md-12 mb-4 mb-lg-0 position-relative"
+            >
+              <PostImage
+                postId={post.id}
+                thumbnailUrl={post.thumbnailUrl}
+                alt={"thumbnail"}
+                showAlert={showAlert}
+              />
+            </div>
+          ))}
         </div>
-      ))}
-      </div>
       </div>
     </div>
-  )
-}
-
-
+  );
+};
 
 export default ProfilePagePhoto;
